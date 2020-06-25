5586a7fde074a49977abef1c234876b65412ea7a
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-25 03:56:33 +0300

    fix trial

--- controllers/persons.js
+++ controllers/persons.js
@@ -2,9 +2,35 @@ const personsRouter = require('express').Router()
 const Person = require('../models/person')
 
 personsRouter.get('/', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons.map(person => person.toJSON()))
-  })
+  Person.find({})
+    .then(persons => {
+      if (persons) {
+        const result = {
+          num: persons.length,
+          date: new Date(),
+        }
+        const results = '<title>Phonebook Info Page</title>'
+        + '<center><br /><strong style=\'font-size: 25px;\'>'
+        + 'Phonebook has info for ' + result.num + ' people'
+        + '</strong><br /><br />' + result.date + '<br /><br />'
+        + '<div className=\'col-auto\' align=\'center\'>'
+        + '<form action=\'/\' method=\'GET\'>'
+        + '<button type=\'submit\' onclick=\'/\'>Back</button>'
+        + '</form></div><br /><hr />'
+        + '<em>Phonebook app, Full Stack Web Development</em><br />'
+        + '<em>University of Helsinki 2020</em><br />'
+        + '<a href=\'https://icons8.com/icon/aggz2cCtJYrP/phone-contact\'>Phone Contact icon by Icons8</a>'
+        + '</center>'
+        response.send(results)
+        response.json(persons.map(person => person.toJSON()))
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(404).end()
+    })
 })
 
 personsRouter.get('/:id', (request, response, next) => {
@@ -56,36 +82,35 @@ personsRouter.put('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-personsRouter.get('/info', (request, response) => {
-  Person.find({})
-    .then(persons => {
-      if (persons) {
-        const result = {
-          num: persons.length,
-          date: new Date(),
-        }
-        const results = '<title>Phonebook Info Page</title>'
-        + '<center><br /><strong style=\'font-size: 25px;\'>'
-        + 'Phonebook has info for ' + result.num + ' people'
-        + '</strong><br /><br />' + result.date + '<br /><br />'
-        + '<div className=\'col-auto\' align=\'center\'>'
-        + '<form action=\'/\' method=\'GET\'>'
-        + '<button type=\'submit\' onclick=\'/\'>Back</button>'
-        + '</form></div><br /><hr />'
-        + '<em>Phonebook app, Full Stack Web Development</em><br />'
-        + '<em>University of Helsinki 2020</em><br />'
-        + '<a href=\'https://icons8.com/icon/aggz2cCtJYrP/phone-contact\'>Phone Contact icon by Icons8</a>'
-        + '</center>'
-        response.send(results)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(404).end()
-    })
-
-})
+// personsRouter.get('/info', (request, response) => {
+//   Person.find({})
+//     .then(persons => {
+//       if (persons) {
+//         const result = {
+//           num: persons.length,
+//           date: new Date(),
+//         }
+//         const results = '<title>Phonebook Info Page</title>'
+//         + '<center><br /><strong style=\'font-size: 25px;\'>'
+//         + 'Phonebook has info for ' + result.num + ' people'
+//         + '</strong><br /><br />' + result.date + '<br /><br />'
+//         + '<div className=\'col-auto\' align=\'center\'>'
+//         + '<form action=\'/\' method=\'GET\'>'
+//         + '<button type=\'submit\' onclick=\'/\'>Back</button>'
+//         + '</form></div><br /><hr />'
+//         + '<em>Phonebook app, Full Stack Web Development</em><br />'
+//         + '<em>University of Helsinki 2020</em><br />'
+//         + '<a href=\'https://icons8.com/icon/aggz2cCtJYrP/phone-contact\'>Phone Contact icon by Icons8</a>'
+//         + '</center>'
+//         response.send(results)
+//       } else {
+//         response.status(404).end()
+//       }
+//     })
+//     .catch(error => {
+//       console.log(error)
+//       response.status(404).end()
+//     })
+// })
 
 module.exports = personsRouter
