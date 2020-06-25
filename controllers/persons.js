bfef0f0547452fc7b5085e35d582947ff4305aa9
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-25 03:46:09 +0300

    trial fix

--- controllers/persons.js
+++ controllers/persons.js
@@ -56,4 +56,36 @@ personsRouter.put('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+personsRouter.get('/info', (request, response) => {
+  Person.find({})
+    .then(persons => {
+      if (persons) {
+        const result = {
+          num: persons.length,
+          date: new Date(),
+        }
+        const results = '<title>Phonebook Info Page</title>'
+        + '<center><br /><strong style=\'font-size: 25px;\'>'
+        + 'Phonebook has info for ' + result.num + ' people'
+        + '</strong><br /><br />' + result.date + '<br /><br />'
+        + '<div className=\'col-auto\' align=\'center\'>'
+        + '<form action=\'/\' method=\'GET\'>'
+        + '<button type=\'submit\' onclick=\'/\'>Back</button>'
+        + '</form></div><br /><hr />'
+        + '<em>Phonebook app, Full Stack Web Development</em><br />'
+        + '<em>University of Helsinki 2020</em><br />'
+        + '<a href=\'https://icons8.com/icon/aggz2cCtJYrP/phone-contact\'>Phone Contact icon by Icons8</a>'
+        + '</center>'
+        response.send(results)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(404).end()
+    })
+
+})
+
 module.exports = personsRouter
