1dec56926e4b0b6846e63a35dabb5d8991cd2e8b
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-25 04:47:25 +0300

    trial fix

--- controllers/persons.js
+++ controllers/persons.js
@@ -2,35 +2,9 @@ const personsRouter = require('express').Router()
 const Person = require('../models/person')
 
 personsRouter.get('/', (request, response) => {
-  Person.find({})
-    .then(persons => {
-      if (persons) {
-        const result = {
-          num: persons.length,
-          date: new Date(),
-        }
-        const results = '<title>Phonebook Info Page</title>'
-        + '<center><br /><strong style=\'font-size: 25px;\'>'
-        + 'Phonebook has info for ' + result.num + ' people'
-        + '</strong><br /><br />' + result.date + '<br /><br />'
-        + '<div className=\'col-auto\' align=\'center\'>'
-        + '<form action=\'/\' method=\'GET\'>'
-        + '<button type=\'submit\' onclick=\'/\'>Back</button>'
-        + '</form></div><br /><hr />'
-        + '<em>Phonebook app, Full Stack Web Development</em><br />'
-        + '<em>University of Helsinki 2020</em><br />'
-        + '<a href=\'https://icons8.com/icon/aggz2cCtJYrP/phone-contact\'>Phone Contact icon by Icons8</a>'
-        + '</center>'
-        response.send(results)
-        response.json(persons.map(person => person.toJSON()))
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(404).end()
-    })
+  Person.find({}).then(persons => {
+    response.json(persons.map(person => person.toJSON()))
+  })
 })
 
 personsRouter.get('/:id', (request, response, next) => {
@@ -111,6 +85,7 @@ personsRouter.put('/:id', (request, response, next) => {
 //       console.log(error)
 //       response.status(404).end()
 //     })
+
 // })
 
 module.exports = personsRouter
