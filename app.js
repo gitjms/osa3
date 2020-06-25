576f6f2262c3802aab15990c1700022be7185282
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-25 10:37:24 +0300

    fix

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ mongoose.connect( config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopolog
     console.log('error connecting to MongoDB:', error.message)
   })
 
-app.use('/', peopleRouter)
-app.use('/info', infoRouter)
-
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(middleware.requestLogger)
 
+app.use('/api/people', peopleRouter)
+app.use('/api', infoRouter)
+
+app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 app.use(middleware.custom_morgan)
