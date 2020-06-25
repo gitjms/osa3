a1df4a13fb244771d53ed6ae60fa52871f958d86
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-25 03:49:09 +0300

    fix trial

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/persons', personsRouter)
+app.use('/info', personsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
