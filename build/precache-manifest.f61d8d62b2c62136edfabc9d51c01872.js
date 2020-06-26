ce71d20af33b9b4992edbb2e825955d22450e015
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-26 09:58:25 +0300

    global gitignore off

---
+++ build/precache-manifest.f61d8d62b2c62136edfabc9d51c01872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26935c5aa44f381a8839cb0f58eb5517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5099f76de6b1898d0357",
+    "url": "/static/css/main.06eb0d66.chunk.css"
+  },
+  {
+    "revision": "7bd547fefb44a7fc5632",
+    "url": "/static/js/2.0b787f2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b787f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5099f76de6b1898d0357",
+    "url": "/static/js/main.225904e2.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
